2ab2eeea4e8f030813dff082f2f92f29105b0f74
Sriram <sriramvaikundaraman@gmail.com>
2019-08-30 13:47:30 +0530

    hi

--- app/src/main/java/com/example/sampleapp1/Welcome.java
+++ app/src/main/java/com/example/sampleapp1/Welcome.java
@@ -3,6 +3,7 @@ package com.example.sampleapp1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,8 +12,11 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Welcome extends AppCompatActivity {
     TextView username;
@@ -21,6 +25,52 @@ public class Welcome extends AppCompatActivity {
     Button button;
     FirebaseAuth firebaseAuth;
     FirebaseUser currentuser;
+    TextView n,a;
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        String id;
+        n=(TextView)findViewById(R.id.textView5);
+        a=(TextView)findViewById(R.id.textView6);
+      //  Toast.makeText(getApplicationContext(),"Now onStart() calls", Toast.LENGTH_LONG).show(); //onStart Called
+        FirebaseUser currentFirebaseUser =FirebaseAuth.getInstance().getCurrentUser();
+        id=currentFirebaseUser.getUid();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("UserDetails/"+id+"/name");
+         DatabaseReference myRef1=database.getReference("UserDetails/"+id+"/age");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+               String value = dataSnapshot.getValue(String.class);
+                n.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.d("SAMPLEAPP","error: "+error);
+            }
+        });
+
+        myRef1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String value = dataSnapshot.getValue(String.class);
+                a.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.d("SAMPLEAPP","error: "+error);
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +83,9 @@ public class Welcome extends AppCompatActivity {
         FirebaseUser user = firebaseAuth.getCurrentUser();
         databaseReference= FirebaseDatabase.getInstance().getReference().child("UserDetails");
         username.setText(user.getEmail());
+
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
